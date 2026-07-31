5a5c77d10fe57f87166bfe38a94abbc605dc7a5c
Yugal <yugalkhati570@gmail.com>
2026-07-31 21:21:23 +0545

    feat: implement fseek on file

--- src/fs/file.h
+++ src/fs/file.h
@@ -25,6 +25,7 @@ struct disk;
 typedef void *(*FS_OPEN_FUNCTION)(struct disk *disk, struct path_part *path, FILE_MODE mode);
 typedef int (*FS_READ_FUNCTION)(struct disk *disk, void *private, uint32_t size, uint32_t nmemb, char *out);
 typedef int (*FS_RESOLVE_FUNCTION)(struct disk *disk);
+typedef int (*FS_SEEK_FUNCTION)(void *private, uint32_t offset, FILE_SEEK_MODE seek_mode);
 
 struct filesystem
 {
@@ -32,6 +33,7 @@ struct filesystem
     FS_RESOLVE_FUNCTION resolve;
     FS_OPEN_FUNCTION open;
     FS_READ_FUNCTION read;
+    FS_SEEK_FUNCTION seek;
 
     char name[20];
 };
