6477d3595f011ceeca28e355131149c8df25c3a3
Yugal <yugalkhati570@gmail.com>
2026-08-01 09:11:46 +0545

    feat: kernel panic mode added

--- src/fs/file.c
+++ src/fs/file.c
@@ -61,6 +61,12 @@ void fs_init()
     fs_load();
 }
 
+static void file_free_descriptor(struct file_descriptor *desc)
+{
+    file_descriptors[desc->index - 1] = 0x00;
+    kfree(desc);
+}
+
 static int file_new_descriptor(struct file_descriptor **desc_out)
 {
     int res = -ENOMEM;
@@ -185,6 +191,52 @@ out:
     {
         res = 0;
     }
+    return res;
+}
+
+int fstat(int fd, struct file_stat *stat)
+{
+    int res = 0;
+    struct file_descriptor *desc = file_get_descriptor(fd);
+    if (!desc)
+    {
+        res = -EIO;
+        goto out;
+    }
+    res = desc->filesystem->stat(desc->disk, desc->private, stat);
+out:
+    return res;
+}
+
+int fclose(int fd)
+{
+    int res = 0;
+    struct file_descriptor *desc = file_get_descriptor(fd);
+    if (!desc)
+    {
+        res = -EIO;
+        goto out;
+    }
+    res = desc->filesystem->close(desc->private);
+    if (res == MARROWOS_ALL_OK)
+    {
+        file_free_descriptor(desc);
+    }
+out:
+    return res;
+}
+
+int fseek(int fd, int offset, FILE_SEEK_MODE whence)
+{
+    int res = 0;
+    struct file_descriptor *desc = file_get_descriptor(fd);
+    if (!desc)
+    {
+        res = -EIO;
+        goto out;
+    }
+    res = desc->filesystem->seek(desc->private, offset, whence);
+out:
     return res;
 }
 
