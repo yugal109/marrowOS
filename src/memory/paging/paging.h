72f97863c13bf3ca3b6f49106a0bac0b0b58f23e
Yugal <yugalkhati570@gmail.com>
2026-08-02 11:03:47 +0545

    feat: basic prototype for task functionality completed

--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -21,6 +21,7 @@ struct paging_4gb_chunk
 
 struct paging_4gb_chunk *paging_new_4gb(uint8_t flags);
 void paging_switch(uint32_t *directory);
+void paging_free_4gb(struct paging_4gb_chunk *chunk);
 void enable_paging();
 uint32_t *paging_4gb_chunk_get_directory(struct paging_4gb_chunk *chunk);
 
