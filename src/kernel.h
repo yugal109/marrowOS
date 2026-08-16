8579bece10ce41a3e48aca4c54b9b45437305785
Yugal <yugalkhati570@gmail.com>
2026-08-16 14:41:50 +0545

    feat: added put char command and the backspace

--- src/kernel.h
+++ src/kernel.h
@@ -10,6 +10,7 @@ void print(const char *str);
 void panic(const char *msg);
 void kernel_page();
 void kernel_registers();
+void terminal_writechar(char c, char color);
 
 #define ERROR(value) (void *)(value)
 #define ERROR_I(value) (int)(value)
