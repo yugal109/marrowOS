aaa48636707e37ff5d958c67fc7824fceebafc9d
Yugal <yugalkhati570@gmail.com>
2026-08-17 10:35:58 +0545

    fix: fixed a bug in streamer.c regarding hte disk streamer read

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,6 +1,7 @@
 #include "streamer.h"
 #include "memory/heap/kheap.h"
 #include "config.h"
+#include <stdbool.h>
 
 struct disk_stream *disk_streamer_new(int disk_id)
 {
@@ -28,7 +29,14 @@ int disk_streamer_read(struct disk_stream *stream, void *out, int total)
     int sector = stream->pos / MARROWOS_SECTOR_SIZE;
     int offset = stream->pos % MARROWOS_SECTOR_SIZE;
 
+    int total_to_read = total;
+    bool overflow = (offset + total_to_read) >= MARROWOS_SECTOR_SIZE;
     char buf[MARROWOS_SECTOR_SIZE];
+
+    if (overflow)
+    {
+        total_to_read -= (offset + total_to_read) - MARROWOS_SECTOR_SIZE;
+    }
 
     int res = disk_read_block(stream->disk, sector, 1, buf);
     if (res < 0)
@@ -36,7 +44,6 @@ int disk_streamer_read(struct disk_stream *stream, void *out, int total)
         goto out;
     }
 
-    int total_to_read = total > MARROWOS_SECTOR_SIZE - offset ? MARROWOS_SECTOR_SIZE - offset : total;
     for (int i = 0; i < total_to_read; i++)
     {
         *(char *)out++ = buf[offset + i];
@@ -44,7 +51,8 @@ int disk_streamer_read(struct disk_stream *stream, void *out, int total)
 
     // Adjust the stream
     stream->pos += total_to_read;
-    if (total > total_to_read)
+
+    if (overflow)
     {
         res = disk_streamer_read(stream, out, total - total_to_read);
     }
