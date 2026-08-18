5328f677e3783052624e714d513b1bc9ccc18a35
Yugal <yugalkhati570@gmail.com>
2026-08-18 23:41:23 +0545

    fix: mermory mapping malloc in stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,13 +5,9 @@
 
 int main(int argc, char **argv)
 {
-    char words[] = "hello how are you";
-    const char *token = strtok(words, " ");
-    while (token)
-    {
-        printf("%s\n", token);
-        token = strtok(NULL, " ");
-    }
+    char *ptr = yreserve(20);
+    strcpy(ptr, "hello world");
+    print(ptr);
     while (1)
     {
     };
